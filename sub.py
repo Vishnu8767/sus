986d22e7042846b1d1195e0f1fd320237d1edc3c
Vishnu8767 <boddu.reddy@s.amity.edu>
2025-07-21 01:53:20 +0530

    i

--- sub.py
+++ sub.py
@@ -1,6 +1,12 @@
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 
+
+
+
 sum_result = a + b
 sum_result = a - b
 print("The sum is:", sum_result)
+print("The sub is:", sub_result)
+@@@@
+^^&&*
